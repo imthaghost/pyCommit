6c9535a9c7e88885ee5f38089e4d29b85bc89bd8
imthaghost <imthaghost@protonmail.com>
2019-09-30 11:39:31 -0700

    [add] git commands

---
+++ commit.py
@@ -0,0 +1,25 @@
+class commit(object):
+    def __init__(self, message):
+        pass
+
+
+class init(object):
+    def __init__(self, path):
+        pass
+
+
+class remote(object):
+    def __init__(self, url):
+        pass
+
+
+class push(object):
+    def __init__(self):
+        pass
+
+
+class status(object):
+    def __init__(self):
+        msg = {
+            'status': ''
+        }
