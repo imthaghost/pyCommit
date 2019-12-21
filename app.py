3705ec386a3ea997a82c206544687b0aee56e36b
imthaghost <imthaghost@protonmail.com>
2019-12-21 08:00:38 -0800

    [update] added docstring

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+"""Flask Web Application"""
+__maintainer__ = 'Gary Frederick'
+__license__ = 'MIT'
+__version__ = '1.0.0'
+# built-in Python Modules
+import os
+# external Python Modules
 from flask import Flask, session, render_template, redirect
