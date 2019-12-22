d6ccd0f072a7f00b10b98e90744a1184c73db9c2
imthaghost <imthaghost@protonmail.com>
2019-12-21 17:19:06 -0800

    [update]

--- app.py
+++ app.py
@@ -6,3 +6,24 @@ __version__ = '1.0.0'
 import os
 # external Python Modules
 from flask import Flask, session, render_template, redirect
+
+app = Flask(__name__)
+host_url = None
+port_num = 5000
+flask_env = os.getenv('FLASK_ENV')
+flask_debug = os.getenv('FLASK_DEBUG')
+
+if flask_env == 'development':
+    host_url = '127.0.0.1'
+else:
+    host_url = '0.0.0.0'
+
+
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
+def index():
+    return 'home'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host=host_url, port=port_num)
