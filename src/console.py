7275e79edb7749900305a876b89bc001809e417e
imthaghost <imthaghost@protonmail.com>
2019-12-21 23:02:02 -0800

    [update] added dattime patterns/password functionality

--- src/console.py
+++ src/console.py
@@ -1,10 +1,22 @@
-import regex
+"""console.py
+"""
+__maintainer__ = 'Gary Frederick'
+__license__ = 'MIT'
+__version__ = '1.0.0'
+
+# built-in Python Modules
 from pprint import pprint
-from art import icon, git
+import regex
+
+# local Python Modules
+from art import git as banner
+
+# external Python Modules
 from PyInquirer import style_from_dict, Token, prompt
 from PyInquirer import Validator, ValidationError
+import colorama
 
-
+# promt styling
 style = style_from_dict({
     Token.QuestionMark: '#E91E63 bold',
     Token.Selected: '#673AB7 bold',
@@ -14,17 +26,9 @@ style = style_from_dict({
 })
 
 
-class PhoneNumberValidator(Validator):
-    def validate(self, document):
-        ok = regex.match(
-            '^([01]{1})?[-.\s]?\(?(\d{3})\)?[-.\s]?(\d{3})[-.\s]?(\d{4})\s?((?:#|ext\.?\s?|x\.?\s?){1}(?:\d+)?)?$', document.text)
-        if not ok:
-            raise ValidationError(
-                message='Please enter a valid phone number',
-                cursor_position=len(document.text))  # Move cursor to end
-
-
 class NumberValidator(Validator):
+    """"""
+
     def validate(self, document):
         try:
             int(document.text)
@@ -34,56 +38,72 @@ class NumberValidator(Validator):
                 cursor_position=len(document.text))  # Move cursor to end
 
 
-print('Hi, welcome to Python Pizza')
+class DateValidator(Validator):
+    """"""
+
+    def validate(self, document):
+        ok = regex.match(
+            '^(1[0-2]|0?[1-9])/(3[01]|[12][0-9]|0?[1-9])/(?:[0-9]{2})?[0-9]{2}$', document.text)
+        if not ok:
+            raise ValidationError(
+                message='Please enter a valid date',
+                cursor_position=len(document.text))  # Move cursor to end
+
+
+print(banner + '\n')
+print('Hi, welcome to pyCommit' + '\n')
 
 questions = [
     {
-        'type': 'confirm',
-        'name': 'toBeDelivered',
-        'message': 'Is this for delivery?',
-        'default': False
-    },
-    {
-        'type': 'input',
-        'name': 'phone',
-        'message': 'What\'s your phone number?',
-        'validate': PhoneNumberValidator
-    },
-    {
         'type': 'list',
-        'name': 'size',
-        'message': 'What size do you need?',
-        'choices': ['Large', 'Medium', 'Small'],
+        'name': 'server',
+        'message': 'Where should this be executed',
+        'choices': ['Local Machine', 'Web-Server'],
         'filter': lambda val: val.lower()
     },
     {
         'type': 'input',
+        'name': 'start',
+        'message': 'Start Date - format(mm/dd/yyyy)',
+        'validate': DateValidator
+    },
+    {
+        'type': 'input',
+        'name': 'end',
+        'message': 'End Date - format(mm/dd/yyyy)',
+        'validate': DateValidator
+    },
+    {
+        'type': 'input',
+        'name': 'username',
+        'message': 'Whats your github username'
+    },
+    {
+        'type': 'password',
+        'name': 'password',
+        'message': 'Whats your github password'
+    },
+
+    {
+        'type': 'input',
         'name': 'quantity',
-        'message': 'How many do you need?',
+        'message': 'How many commits do you want?',
         'validate': NumberValidator,
         'filter': lambda val: int(val)
     },
     {
-        'type': 'rawlist',
-        'name': 'beverage',
-        'message': 'You also get a free 2L beverage',
-        'choices': ['Pepsi', '7up', 'Coke']
-    },
-    {
         'type': 'input',
         'name': 'comments',
-        'message': 'Any comments on your purchase experience?',
+        'message': 'Any comments on your experience?',
         'default': 'Nope, all good!'
     },
     {
         'type': 'list',
-        'name': 'prize',
-        'message': 'For leaving a comment, you get a freebie',
-        'choices': ['cake', 'fries'],
+        'name': 'ending',
+        'message': 'Thanks for leaving a comment!',
         'when': lambda answers: answers['comments'] != 'Nope, all good!'
     }
 ]
-
-answers = prompt(questions, style=style)
-print('Order receipt:')
-pprint(answers)
+if __name__ == "__main__":
+    answers = prompt(questions, style=style)
+    pprint(answers)
